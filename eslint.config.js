b08ccc7f1cde35637a9dc386b93afe1fa18264d0
Yuliuus <yuliiamarkovska@gmail.com>
2024-08-17 21:52:01 +0300

    1.2

--- eslint.config.js
+++ eslint.config.js
@@ -13,12 +13,14 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    settings: { react: { version: '18.3' } },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
